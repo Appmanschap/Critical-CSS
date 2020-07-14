c341b77dad304ef6eafd96f66370edd223016ea2
Aal <almeric@appmanschap.nl>
2020-07-14 16:57:53 +0200

    Use correct args

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const main = async () => {
             ? input.destinationPath.substr(0, -1)
             : input.destinationPath,
         dest: `${input.syncOptions.sshHost}:${input.syncOptions.targetDir}`,
-        sshCmdArgs: ['-azhcvv'],
+        args: ['-azhcvv'],
         privateKey: input.syncOptions.sshPrivateKeyPath,
         delete: true,
         ssh: true,
